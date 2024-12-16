8b8a3a015ef519a413a961337ff2249bd99f10d5
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-16 14:13:22 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,4 @@
 import { PostContextProvider } from "./assets/contexts/PostContext";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./assets/layout/Header";
+import Main from "./assets/layout/Main";
 
-import PostsPage from "./assets/pages/PostsPage";
-
-export default function App() {
-  return (
-    <PostContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index Component={PostsPage} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </PostContextProvider>
-  );
-}
