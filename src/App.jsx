bc9626f0d499b51f4ede953777baa3841cb60efa
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-16 16:27:32 +0100

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
-import { PostContextProvider } from "./assets/contexts/PostContext";
+import { MovieTvContextProvider } from "./assets/contexts/MovieTvContext";
 import Header from "./assets/layout/Header";
 import Main from "./assets/layout/Main";
 
+export default function App() {
+  return (
+    <MovieTvContextProvider>
+      <div className="wrapper" data-bs-theme="dark">
+        <Header />
+        <Main />
+      </div>
+    </MovieTvContextProvider>
+  );
+}
