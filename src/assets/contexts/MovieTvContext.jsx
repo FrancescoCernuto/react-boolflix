6c8c7273320c31e9541cc48a731de4464cee6e73
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-16 14:32:28 +0100

    import movie

--- src/assets/contexts/MovieTvContext.jsx
+++ src/assets/contexts/MovieTvContext.jsx
@@ -2,3 +2,10 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 // Context Consumer
 const MovieTvContext = createContext();
+export const UseMovieTvContext = () => useContext(MovieTvContext);
+
+//  Context Provider
+export function MovieTvContextProvider({ children }) {
+    const movieUrl = import.meta.env.VITE_API_URL_MOVIE;
+}
+
