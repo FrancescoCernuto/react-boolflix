0fed4a1d0e9c32afbe10e43a4a43e3c8b63ce626
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-17 17:09:10 +0100

    add rating star

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,3 +1,22 @@
 export default function Card() {
-    return <h1>Card</h1>;
-}
+    const VITE_POSTER_URL = "https://image.tmdb.org/t/p";
+    const VITE_POSTER_SIZE = "/w342/";
+    const posterOrigin = VITE_POSTER_URL;
+    const posterSize = VITE_POSTER_SIZE;
+    const posterPath = item.poster_path;
+    let posterUrl;
+    if (posterPath) posterUrl = posterOrigin + posterSize + posterPath;
+
+
+    function raiting(vote) {
+        if (!vote || vote < 0) vote = 0;
+        const stars = Math.round(vote / 2);
+        const totalStars = 5;
+        const starsArr = [];
+        for (let i = 0; i < totalStars; i++) {
+            i < stars
+                ? starsArr.push(<i key={i} className="fa-solid fa-star"></i>)
+                : starsArr.push(<i key={i} className="fa-regular fa-star"></i>);
+
+        }
+    }
